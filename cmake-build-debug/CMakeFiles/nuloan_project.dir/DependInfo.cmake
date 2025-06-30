a2e290340ae446ff3b266cae2d3b5b42eb1ae1fa
MMILHA <98252587+MossabMilha@users.noreply.github.com>
2025-06-30 22:31:54 +0100

    little changes

---
+++ cmake-build-debug/CMakeFiles/nuloan_project.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Active_Member.c" "CMakeFiles/nuloan_project.dir/Active_Member.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Active_Member.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Apply_Loan.c" "CMakeFiles/nuloan_project.dir/Apply_Loan.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Apply_Loan.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Apply_Loan_More_Information.c" "CMakeFiles/nuloan_project.dir/Apply_Loan_More_Information.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Apply_Loan_More_Information.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Apply_Loan_fianl.c" "CMakeFiles/nuloan_project.dir/Apply_Loan_fianl.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Apply_Loan_fianl.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Approve_Reject_Application.c" "CMakeFiles/nuloan_project.dir/Approve_Reject_Application.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Approve_Reject_Application.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Auto_Check_application.c" "CMakeFiles/nuloan_project.dir/Auto_Check_application.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Auto_Check_application.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Bank_information.c" "CMakeFiles/nuloan_project.dir/Bank_information.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Bank_information.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_Bank_Information.c" "CMakeFiles/nuloan_project.dir/Check_Bank_Information.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_Bank_Information.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_Loan_Status.c" "CMakeFiles/nuloan_project.dir/Check_Loan_Status.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_Loan_Status.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_Loan_Status_more.c" "CMakeFiles/nuloan_project.dir/Check_Loan_Status_more.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_Loan_Status_more.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_Signup_Information.c" "CMakeFiles/nuloan_project.dir/Check_Signup_Information.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_Signup_Information.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_User_Information.c" "CMakeFiles/nuloan_project.dir/Check_User_Information.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_User_Information.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Check_email.c" "CMakeFiles/nuloan_project.dir/Check_email.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Check_email.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/First_Page.c" "CMakeFiles/nuloan_project.dir/First_Page.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/First_Page.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Forget_Password.c" "CMakeFiles/nuloan_project.dir/Forget_Password.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Forget_Password.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Login.c" "CMakeFiles/nuloan_project.dir/Login.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Login.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Login_Process_Handle_Window.c" "CMakeFiles/nuloan_project.dir/Login_Process_Handle_Window.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Login_Process_Handle_Window.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Save_file.c" "CMakeFiles/nuloan_project.dir/Save_file.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Save_file.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Send_Email.c" "CMakeFiles/nuloan_project.dir/Send_Email.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Send_Email.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Show_Loan_Application.c" "CMakeFiles/nuloan_project.dir/Show_Loan_Application.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Show_Loan_Application.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Show_Users.c" "CMakeFiles/nuloan_project.dir/Show_Users.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Show_Users.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/SignUp.c" "CMakeFiles/nuloan_project.dir/SignUp.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/SignUp.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/Super_Admin.c" "CMakeFiles/nuloan_project.dir/Super_Admin.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/Super_Admin.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/hash_password.c" "CMakeFiles/nuloan_project.dir/hash_password.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/hash_password.c.obj.d"
+  "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/main.c" "CMakeFiles/nuloan_project.dir/main.c.obj" "gcc" "CMakeFiles/nuloan_project.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/PC/CLionProjects/nuloan
+# Install script for directory: C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/PC/CLionProjects/nuloan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/PC/Desktop/Projects_Academic/C/Nuloan_App/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
